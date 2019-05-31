fc3117f545dcd5637b850d127b7dc853c4c21908
andb70 <andb70@gmail.com>
2019-05-31 20:20:21 +0200

    7 segment display encoded

--- SerialCom4PRx.X/main.c
+++ SerialCom4PRx.X/main.c
@@ -7,6 +7,8 @@
 
 #include <p32xxxx.h> // Include PIC32 specifics header file
 #include <plib.h> // Include the PIC32 Peripheral Library
+#include <stdio.h>
+#include <stdlib.h>
 
 // DEVCFG2
 #pragma config FPLLIDIV = DIV_2 // PLL Input Divider (1x Divider)
@@ -45,6 +47,11 @@
 #define T1_TICK       		(SYS_FREQ/PB_DIV/PRESCALE/TOGGLES_PER_SEC)
 #define SYSCLK 80000000L // Give the system?s clock frequency
 
+// rising edge: when port is 0 and oldport is 1
+#define rTrig(port, btn, old) ((~port & btn) & (old & btn))
+// falling edge: when port is 1 and oldport is 0
+#define fTrig(port, btn, old) ((port & btn) & (~old & btn))
+
 #define mapA 1
 #define mapB 2
 #define mapping 1
@@ -59,16 +66,16 @@
  *	5       D4              RD6     DataRX		DataRX
  *	6       D5              RD7			
  *	7       D6              RD8     btn4Up		
- *	8       D7              RD11	btn4Down	Disp1
+ *	8       D7              RD11	btn4Down	Disp1   a
  *						
  *	CON5 ? DIGITAL:			PORT    A           B
  *	Pin #	Signal Name				
- *	1       D8_MMC_#SS      RB13	btn3Up		Disp2
- *	2       D9              RB14	btn3Down	Disp3
- *	3       D10(#SS)        RG9     btn2Up		Disp4
- *	4       D11(MOSI)       RG8     btn2Down	Disp5
- *	5       D12(MISO)       RG7     btn1Up		Disp6
- *	6       D13(SCK/LED1)	RG6     btn1Down	Disp7
+ *	1       D8_MMC_#SS      RB13	btn3Up		Disp2   b
+ *	2       D9              RB14	btn3Down	Disp3   c
+ *	3       D10(#SS)        RG9     btn2Up		Disp4   g
+ *	4       D11(MOSI)       RG8     btn2Down	Disp5   f
+ *	5       D12(MISO)       RG7     btn1Up		Disp6   e
+ *	6       D13(SCK/LED1)	RG6     btn1Down	Disp7   d
  *	7       GND				
  *	8       AREF				
  *						
@@ -81,58 +88,77 @@
  *	5       A4(SDA1)				
  *	6       A5(SCL1)				
  */
+/*	7 segments Display
+ * 									
+ *		  --a--							
+ *		|		|						
+ *		f		b						
+ *		|		|						
+ *		  --g--							
+ *		|		|						
+ *		e		c						
+ *		|		|						
+ *		  --d--							
+ */										
+static const unsigned char display7s[] = {										
+	0x7E,	0x30,	0x6D,	0x79,		//	0	1	2	3
+	0x33,	0x5B,	0x5F,	0x70,		//	4	5	6	7
+	0x7F,	0x73,	0x77,	0x1F,		//	8	9	A	b
+	0x4E,	0x3D,	0x4F,	0x47		//	C	d	E	F
+};										
 
-void initializePORTOUT() {
+char timerElapsed = 0;
+
+void showOnDisplay(unsigned int indexArray) {
+    char dato = display7s[indexArray];
+//	RD11    Disp1   a   6 >> 11
+    LATD = (LATD & 0xF7FF) | ((dato & 0xFFBF) << 5); // a
+//	RB13	Disp2   b   5 >> 14
+//	RB14	Disp3   c   4 >> 13 
+    LATB = (LATB & 0x9FFF) | ((dato & 0xFFCF) << 9); //  bc
+//	RG9     Disp4   g   3 >> 9
+//	RG8     Disp5   f   2 >> 8
+//	RG7     Disp6   e   1 >> 7
+//	RG6     Disp7   d   0 >> 6
+    LATG = (LATG & 0xFC3F) | ((dato & 0xFFF0) << 6); //    gfed
+}
+
+
+
+void initializePORTIO() {
     // configure PORTs
+    
+    PORTSetPinsDigitalOut(IOPORT_D, BIT_2); //Configure pin as output. Clock TX
+    PORTSetPinsDigitalIn(IOPORT_D, BIT_3); // Configure pin as input. Clock RX    
+    PORTSetPinsDigitalOut(IOPORT_D, BIT_5); //Configure pin as output. Data TX
+    PORTSetPinsDigitalIn(IOPORT_D, BIT_6); // Configure pin as input. Data RX
+ 
+    mJTAGPortEnable(0); // Disable JTAG
     if (mapping == mapA) {
-        PORTSetPinsDigitalIn(IOPORT_B, BIT_1); // Configure pin as input.
-        PORTSetPinsDigitalIn(IOPORT_B, BIT_2); // Configure pin as input.
-        PORTSetPinsDigitalIn(IOPORT_B, BIT_3); // Configure pin as input.
-        PORTSetPinsDigitalIn(IOPORT_B, BIT_4); // Configure pin as input.
-
-        PORTSetPinsDigitalOut(IOPORT_D, BIT_5); // Configure pin as output.
-        PORTSetPinsDigitalOut(IOPORT_D, BIT_6); // Configure pin as output.
-        PORTSetPinsDigitalOut(IOPORT_D, BIT_7); // Configure pin as output.
-        PORTSetPinsDigitalOut(IOPORT_D, BIT_8); // Configure pin as output.
-        PORTSetPinsDigitalOut(IOPORT_D, BIT_11); // Configure pin as output.
-        mJTAGPortEnable(0); // Disable JTAG
-        PORTSetPinsDigitalOut(IOPORT_B, BIT_13); // Configure pin as output.    
-        PORTSetPinsDigitalOut(IOPORT_B, BIT_14); // Configure pin as output.
-
-        PORTSetPinsDigitalOut(IOPORT_G, BIT_9); // Configure pin as output.
+        PORTSetPinsDigitalIn(IOPORT_D, BIT_8); // Configure pin as input. btn4Up
+        PORTSetPinsDigitalIn(IOPORT_D, BIT_11); //Configure pin as input. btn4Down
+        PORTSetPinsDigitalIn(IOPORT_B, BIT_13); //Configure pin as input. btn3Up
+        PORTSetPinsDigitalIn(IOPORT_B, BIT_14); //Configure pin as input. btn3Down
+        PORTSetPinsDigitalIn(IOPORT_G, BIT_9); // Configure pin as input. btn2Up
+        PORTSetPinsDigitalIn(IOPORT_G, BIT_8); // Configure pin as input. btn2Down
+        PORTSetPinsDigitalIn(IOPORT_G, BIT_7); // Configure pin as input. btn1Up
+        PORTSetPinsDigitalIn(IOPORT_G, BIT_6); // Configure pin as input. btn1Down
     }
-
+    else if (mapping == mapB) {
+        PORTSetPinsDigitalOut(IOPORT_D, BIT_11); //Configure pin as output. Disp1
+        PORTSetPinsDigitalOut(IOPORT_B, BIT_13); //Configure pin as output. Disp2
+        PORTSetPinsDigitalOut(IOPORT_B, BIT_14); //Configure pin as output. Disp3
+        PORTSetPinsDigitalOut(IOPORT_G, BIT_9); // Configure pin as output. Disp4
+        PORTSetPinsDigitalOut(IOPORT_G, BIT_8); // Configure pin as output. Disp5
+        PORTSetPinsDigitalOut(IOPORT_G, BIT_7); // Configure pin as output. Disp6
+        PORTSetPinsDigitalOut(IOPORT_G, BIT_6); // Configure pin as output. Disp7
+    }
 
     LATB = 0;
     LATD = 0;
     LATG = 0;
 }
 
-void showDato(unsigned int dato) {
-
-    // segnala solo i primi 8 bit
-    LATD = (LATD & 0xF61F) | ((dato & 0x000F) << 5) | ((dato & 0x0010) << 7);
-    /* equivale a:
-    LATDbits.LATD5 = getBit(dato, 0);
-    LATDbits.LATD6 = getBit(dato, 1);
-    LATDbits.LATD7 = getBit(dato, 2);
-    LATDbits.LATD8 = getBit(dato, 3);
-    LATDbits.LATD11 = getBit(dato, 4);*/
-
-
-    LATB = (LATB & 0x9FFF) | ((dato & 0x0060) << 8);
-    /* equivale a
-    LATBbits.LATB13 = getBit(dato, 5);
-    LATBbits.LATB14 = getBit(dato, 6);*/
-
-
-    LATG = (LATG & 0xFDFF) | ((dato & 0x0080) << 2);
-    /* equivale a
-    LATGbits.LATG9 = getBit(dato, 7); */
-}
-
-#include <stdio.h>
-#include <stdlib.h>
 
 /*
  * 
@@ -161,7 +187,7 @@ int main() {
     SYSTEMConfigPerformance(SYSCLK);
     TRISB = 0x0002;
 
-    initializePORTOUT();
+    initializePORTIO();
 
     while (1) {
 
@@ -177,5 +203,5 @@ void __ISR(_TIMER_1_VECTOR, IPL2SOFT) Timer1Handler(void) {
     mT1ClearIntFlag();
 
     // set the notify bit
-    timer_event = 1;
+    timerElapsed = 1;
 }
