edb455be3fe60115526cde80944d6ed7208c81a5
andb70 <andb70@gmail.com>
2019-05-31 15:37:34 +0200

    TxAdd

---
+++ SerialCom4PTx.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=SerialCom4PTx.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/SerialCom4PTx.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=serialcom4ptx.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/serialcom4ptx.x.tar
